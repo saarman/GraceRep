76eca875b0dfef79fe2ef2f06c8bc89467b851d4
saarman <norah.saarman@yale.edu>
2018-12-10 15:56:13 -0500

    01a CreateSpatialLines 2018-11-30 R scripts

---
+++ ARIDITY/01b.copyTIF.2018-12-10.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#SBATCH --output=01b.copyTIF.2018-12-10.sh.log
+cp /project/fas/powell/esp38/dataproces/MOSQLAND/consland/ARIDITY/AI_annual.tif /gpfs/loomis/home.grace/fas/caccone/nps25/project/ARIDITY/AI_annual.tif
